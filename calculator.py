08310d7803977bd63b4ae66ff257903875aa324a
sireeshajangam <111105961+sireeshajangam@users.noreply.github.com>
2022-10-13 11:09:37 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -29,7 +29,38 @@ name : tim
 birthday : 1990-03-10
 job : Software Engineer
 
-3.       
+3. 3..........using dictionaries....... 
+employees = [{
+  "name": "Tina",
+  "age": 30,
+  "birthday": "1990-03-10",
+  "job": "DevOps Engineer",
+  "address": {
+    "city": "New York",
+    "country": "USA"
+  }
+},
+{
+  "name": "Tim",
+  "age": 35,
+  "birthday": "1985-02-21",
+  "job": "Developer",
+  "address": {
+    "city": "Sydney",
+    "country": "Australia"
+  }
+}
+]
+print(type(employees))
+for k in range (len(employees)):
+      print("name", ':' , employees[k]["name"])
+      print("job", ':', employees[k]["job"])
+      print("city", ':', employees[k]["address"]["city"])
+print(employees[1]["address"]["country"])
+
+output:
+
+name : Tina
 
 
 
